34d366d124c850ca41fc176df6d70076056a5f44
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-16 17:01:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+
+const driver = {driverName:'SAM'};
+
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  const newClone ={...driver};
+  newClone[key]=value;
+  return newClone;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key]=value;
+  return driver;
+}
