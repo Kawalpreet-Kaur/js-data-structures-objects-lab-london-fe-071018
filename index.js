aa11f0ca6fdc777eb0557a40b2e3b4d80e588a79
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-16 17:25:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,10 +21,6 @@ function deleteFromDriverByKey(driver, key){
 } 
 
 function destructivelyDeleteFromDriverByKey(driver, key){
-  // const newClone3=Object.assign({},driver);
-  // delete newClone3[key];
-  // return newClone3;
-  
   delete driver[key];
   return driver;
 }
